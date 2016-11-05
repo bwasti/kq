95bcd743cda8e7dc918db814724f0e0c38be04e4
Ryan Catoen <rmcatoen@gmail.com>
2016-11-05 16:09:33 -0700

    Added hitbox class

---
+++ src/hitbox.h
@@ -0,0 +1,21 @@
+namespace killerqueen {
+
+class Hitbox {
+
+public:
+  Hitbox(Point bl, Point tr) {
+    bottom_left_ = bl;
+    top_right_ = tr;
+  }
+  HasCollision(Point bl, Point tr) {
+    return bl.x >= bottom_left_.x &&
+           tr.x <= tr.x &&
+           bl.y >= bottom_left_.y &&
+           tr.y <= top_right_.y;
+  }
+
+private:
+  Point bottom_left_;
+  Point top_right_;
+};
+} // namespace killerqueen
